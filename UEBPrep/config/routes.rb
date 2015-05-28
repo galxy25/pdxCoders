7caa55f8f44d73ca21d0d064227cb97ac24b7bb5
Brandon Isom <brandon.g.isom@pdx.edu>
2015-05-28 14:49:22 -0700

    [#95391718] Adding generated scaffold for Users

--- UEBPrep/config/routes.rb
+++ UEBPrep/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :users
+
   root 'homepage#index'
 
   get 'homepage/index'
---
+++ UEBPrep/spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :user do
+    username "MyString"
+email "MyString"
+  end
+
+end
