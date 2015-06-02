5197e5635ecb38fadfdde237972c482e22444316
caesarfish <schottec@gmail.com>
2015-06-01 19:26:45 -0700

    Added login page
    login not yet implemented

---
+++ UEBPrep/app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    #if user && user.authenticate(params[:session][:password])
+    if user
+      # Log the user in and redirect to the user's show page.
+    else
+      # Create an error message.
+      render 'new'
+    end
+  end
+end
--- UEBPrep/config/routes.rb
+++ UEBPrep/config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   root 'homepage#index'
   get 'signup/' => 'users#new'
   get 'users/new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
 
   resources :users
 
