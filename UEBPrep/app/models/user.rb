a049c31e0f3d5b459b129433efe8647b713eb4d8
caesarfish <schottec@gmail.com>
2015-06-01 18:28:21 -0700

    Added user signup page

--- UEBPrep/app/models/user.rb
+++ UEBPrep/app/models/user.rb
@@ -6,5 +6,5 @@ class User < ActiveRecord::Base
       format: { with: VALID_EMAIL_REGEX },
       uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 7, maximum: 20 }
-  
+
 end
--- UEBPrep/config/routes.rb
+++ UEBPrep/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
   root 'homepage#index'
+  get 'signup/' => 'users#new'
+  get 'users/new'
 
-  get 'homepage/index'
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
