2fb1cb20455e4af10da32d73a7eaa69a986cd93c
Raven Allan <raven2@pdx.edu>
2015-06-02 20:46:56 -0700

    [#95392370] added updates to user auth/creation - still not finished tho

--- UEBPrep/app/assets/javascripts/homepage.js
+++ UEBPrep/app/assets/javascripts/homepage.js
@@ -1,67 +1,119 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
 /*****************************************************************************
  *
- * Name:             main.js
+ * Name:            main.js
  *
- * Description:      Source Script for the pdxCoders eLearning Platform Project
+ * Description:     Source Script for the pdxCoders eLearning Platform Project
  *
+ *                  All behaviors and hooks related to matching controllers
+ *                  go here; logic automatically available via application.js
  * Log:
  *
- * 0.0  2015-05-25   Initial Revision
+ * 0.0  2015-05-25  Initial Revision
  *
- * Copyright:        pdxCoders (c) 2015 0.0 @ http:// ... .com
+ * 0.1  2015-06-01  Implemented User creation & authentication functionality
+ *
+ * Copyright:       pdxCoders (c) 2015 0.0 @ http:// ... .com
  *****************************************************************************/
 var
-    mainObj = {},
+    mainObj   = {},
     windowObj = { winH:$(window).height(), winW:$(window).width() }
     ;
 
 
 /*****************************************************************************
  *
- * Function:     window.onload
+ * Function:    $(document).ready()
  *
- * Description:  Execute after all content has been loaded
+ * Description: Execute after all content has been loaded
+ *
+ *****************************************************************************/
+$(function()
+{
+    console.log("DOM loaded; likely add shit here later..");
+});
+
+
+/*****************************************************************************
+ *
+ * Function:    window.onload()
+ *
+ * Description: Execute after all content has been loaded
  *
  *****************************************************************************/
 window.onload = function()
 {
-
-    // Load main content...
-
-
-    // Resize main content to fit screen appropriately
     $(window).resize(function()
     {
-        windowObj.updateHW();
-        resizeFont();
-
-        var iconHW = "height:";
-        iconHW += windowObj.winH * 0.12, iconHW += "px;width:auto;";
-        $("#iNav").attr("style", iconHW);
+        windowObj.updateHW().resizeContent(); // add timer later to prevent constant firing
     });
-    $(window).resize();
+    $(window).resize();     // fire resize on main app init
+    $('#homepage-homeBtn, #homepage-mainTbl').fadeIn(400);
 
-    function resizeFont()
-    {
-        /* Resize Font Size
-         var body = $('body'), maxScale = 95, minScale = 30, fontSize = body.width() * 0.065;
-         if (fontSize > maxScale) fontSize = maxScale;
-         if (fontSize < minScale) fontSize = minScale;
-         body.css('font-size', fontSize + '%'); */
-        var fontSize, body = $("body");
-        fontSize = body.width() * 0.075, fontSize = (fontSize > 95) ? 95 : (fontSize < 30) ? 30 : fontSize, fontSize += "%";
-        body.css("font-size", fontSize);
-    }
+    mainObj.appStart();     // run main application manager
 };
 
 
 /*****************************************************************************
  *
- * Function:     windowObj.updateHW()
+ * Function:    mainObj.appStart()
  *
- * Description:  Updates windowObj Namespace vars of window's height/width
+ * Description: Main Application Manager
+ *
+ *****************************************************************************/
+mainObj.appStart = function()
+{
+    $('#homepage-joinBtnTwitter')
+    .mouseenter(function() { $(this).html("join with twitter"); })
+    .mouseleave(function() { $(this).html('<img id="homepage-iTwitter" src="assets/homepage/iTwitter.png" style="height:90%;width:auto;" alt="image">'); })
+    .click(function()
+    {
+        alert("nice try, but this isn't implemented yet...");
+    });
+
+    $('#homepage-joinBtnFacebook')
+    .mouseenter(function() { $(this).html("join with facebook"); })
+    .mouseleave(function() { $(this).html('<img id="homepage-iFacebook" src="assets/homepage/iFacebook.png" style="height:100%;width:auto;" alt="image">'); })
+    .click(function()
+    {
+        alert("nice try, but this isn't implemented yet...");
+    });
+}
+
+
+/*****************************************************************************
+ *
+ * Function:    windowObj.resizeContent()
+ *
+ * Description: Resize content to fit window screen size appropriately
+ *
+ * Notes:       Font size is modified accordingly:
+ *
+ *              if (fontSize > maxScale) fontSize = maxScale;
+ *              if (fontSize < minScale) fontSize = minScale;
+ *              body.css('font-size', fontSize + '%');
+ *
+ *****************************************************************************/
+windowObj.resizeContent = function()
+{
+    var fontSize, hw = "height:", body = $('body');
+    hw += this.winH * 0.08, hw += "px;width:auto;";
+    $('#homepage-iHeader, #homepage-iHomeBtn').attr('style', hw);
+    $('#homepage-mainContent').height(this.winH * 0.58).width(this.winW * 0.35);
+
+    var mainContentH = $('#homepage-mainContent').height();
+    $('#homepage-joinBtnFacebook, #homepage-joinBtnTwitter, .homepage-input').height(mainContentH * 0.045).width(mainContentH * 0.4);
+
+    fontSize = body.width() * 0.06, fontSize = (fontSize > 95) ? 95 : (fontSize < 30) ? 30 : fontSize, fontSize += "%";
+    body.css('font-size', fontSize);
+    return this;
+}
+
+
+/*****************************************************************************
+ *
+ * Function:    windowObj.updateHW()
+ *
+ * Description: Updates windowObj Namespace vars of window's height/width
  *
  *****************************************************************************/
 windowObj.updateHW = function()
