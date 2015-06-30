21f34fecbe65b90f80f86d0720ccb65101425d24
Elle <ellecdwt@gmail.com>
2015-06-29 21:48:43 -0700

    added the get to routes for the url /cardcreator to load the page

--- UEBPrep/config/routes.rb
+++ UEBPrep/config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   get    'signup'  => 'users#new'
   get    'login'   => 'sessions#new'
+  get    'cardcreator' => 'cards#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
 
