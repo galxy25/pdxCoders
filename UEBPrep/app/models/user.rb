577edb44dcfb5b899bb50b15f1e955002d9f39d1
Levi Schoen <levi@scratch-it.com>
2015-06-29 17:00:05 -0700

    [#95758894] Code Review changes -Remove custom user model email/password validations(handle with devise) -Remove password from user account view/substitute with user's API_Key

--- UEBPrep/app/models/user.rb
+++ UEBPrep/app/models/user.rb
@@ -3,14 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  before_save { self.email = email.downcase }
-  #validates :name,  presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-            format: { with: VALID_EMAIL_REGEX },
-            uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 7, maximum: 20 }
-
   acts_as_paranoid
 
   extend FriendlyId
