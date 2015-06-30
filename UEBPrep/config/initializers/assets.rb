5e3643477f27a0061d19b21adfc8907240f331ae
Levi Schoen <levi@scratch-it.com>
2015-06-29 20:06:39 -0700

    [#98072718] Turn off turbo links -Jquery & turbo links conflict with document.ready -Lay the ground work for rendering only controller specific .css and .js files.

--- UEBPrep/config/initializers/assets.rb
+++ UEBPrep/config/initializers/assets.rb
@@ -6,3 +6,13 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+
+#Comment this out if you want to only serve the controller specific .js and .css files for each view
+# %w( users sessions homepage ).each do |controller|
+#   Rails.application.config.assets.precompile += ["#{controller}.js", "#{controller}.css"]
+# end
+#And add these lines to your layouts/application.html.erb
+# <%= stylesheet_link_tag "application", params[:controller], :media => "all" %>
+# <%= javascript_include_tag "application", params[:controller] %>
+#As well as removing the require_tree . from application{.css, .jss}
