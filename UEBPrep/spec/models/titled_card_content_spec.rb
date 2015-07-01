68de0da9b9d61f310ca092478777d0ea32c7edb1
Brandon Isom <brandon.g.isom@pdx.edu>
2015-07-01 09:05:04 -0700

    [#95759182] Add tests for titled card content creation.

--- UEBPrep/spec/models/titled_card_content_spec.rb
+++ UEBPrep/spec/models/titled_card_content_spec.rb
@@ -1,5 +1,18 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe TitledCardContent, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "Create" do
+    it "creates a corresponding Card entry" do
+      content = TitledCardContent.new(title: "content title", text: "content text")
+      expect{content.save}.to change{Card.count}.by 1
+    end
+
+    it "creates a card entry with the proper content_type_id and content_id" do
+      content = TextContent.new(title: "content title", text: "content text")
+      content.save
+      card = Card.find_by content_id: content.id
+      expect(card.content_id).to equal content.id
+    end
+  end
 end
