02ca01ac0051ee6b8f21bdb0db9fc029b7936b47
Brandon Isom <brandon.g.isom@pdx.edu>
2015-07-01 14:06:53 -0700

    [#95759182] Added a test

--- UEBPrep/spec/models/card_spec.rb
+++ UEBPrep/spec/models/card_spec.rb
@@ -1,5 +1,17 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe Card, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "Create" do
+
+    it "does not initialize a card with a non-existant content" do
+      expect {
+        card = Card.new(content_type_id: 1, content_id: 1, created_by: 1)
+      }
+    end
+    
+    # TODO: think of more tests.
+    # Kind of awkward because of the necessity for a card entry to be created
+    # by a content object on save.
+  end
 end
