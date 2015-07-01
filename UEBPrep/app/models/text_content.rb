054e70da4c4c2633d9c2ff49a7f83b82008f7cd1
Brandon Isom <brandon.g.isom@pdx.edu>
2015-07-01 07:40:08 -0700

    [#95759182] Added partial path to text content. Initial commit of titled card content with a partial path as well.

--- UEBPrep/app/models/text_content.rb
+++ UEBPrep/app/models/text_content.rb
@@ -1,2 +1,5 @@
 class TextContent < ActiveRecord::Base
+  def to_partial_path
+    'text_content'
+  end
 end
---
+++ UEBPrep/app/models/titled_card_content.rb
@@ -0,0 +1,5 @@
+class TitledCardContent < ActiveRecord::Base
+  def to_partial_path
+    'titled_card_content'
+  end
+end
