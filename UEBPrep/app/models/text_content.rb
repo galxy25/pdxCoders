d30fcb4577fd31225effd98276572deed2776502
Brandon Isom <brandon.g.isom@pdx.edu>
2015-07-01 08:58:02 -0700

    [#95759182] Add after_save callback to text content for creating an association in cards table

--- UEBPrep/app/models/text_content.rb
+++ UEBPrep/app/models/text_content.rb
@@ -1,5 +1,12 @@
 class TextContent < ActiveRecord::Base
+  after_save   :create_card
+
   def to_partial_path
     'text_content'
   end
+
+  private
+    def create_card
+      Card.create(content_type_id: 1, content_id: self.id)
+    end
 end
