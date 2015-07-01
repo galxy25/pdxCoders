1a79f242babd91e2373258a22d03036b4104fbe3
Brandon Isom <brandon.g.isom@pdx.edu>
2015-07-01 07:36:07 -0700

    [#95759182] Added content attribute and basic content initializer callback.

--- UEBPrep/app/models/card.rb
+++ UEBPrep/app/models/card.rb
@@ -1,4 +1,21 @@
 class Card < ActiveRecord::Base
   validates :content_type_id, presence: true
   validates :content_id, presence: true
+
+  attr_accessor :content
+
+  after_initialize :get_content
+  after_find       :get_content
+
+  private
+    # I hate everything about needing to do this ...
+    # something not quite right with my schema
+    def get_content
+      case self.content_type_id
+        when 1
+          @content = TextContent.find(self.content_id)
+        when 2
+          @content = TitledCardContent.find(self.content_id)
+      end
+    end
 end
