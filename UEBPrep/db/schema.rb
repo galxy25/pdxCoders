35e2da7d495191d6df0d1bb21c5cff36a1bf7a08
parkerBerger <parker_berger@yahoo.com>
2015-07-01 13:03:54 -0700

    7-1-15 (1303)

--- UEBPrep/db/schema.rb
+++ UEBPrep/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150625025611) do
+ActiveRecord::Schema.define(version: 20150701194003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,8 @@ ActiveRecord::Schema.define(version: 20150625025611) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["deleted_at"], name: "index_users_on_deleted_at", using: :btree
