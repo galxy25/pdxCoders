ab01085c51302f303dfb24269cdd3c4490fbfd76
Brandon Isom <brandon.g.isom@pdx.edu>
2015-07-01 10:31:28 -0700

    [#95759182] Added migration for created_by field in cards

--- UEBPrep/db/seeds.rb
+++ UEBPrep/db/seeds.rb
@@ -7,15 +7,15 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-text_content = TextContent.create(text: 'text content woohooo!')
-content_type = ContentType.create(name: 'TextContent')
-Card.create(content_type_id: content_type.id, content_id: text_content.id)
-
-titled_card_content = TitledCardContent.create(title: 'super cool title', text: 'double super cool text')
-content_type = ContentType.create(name: 'TitledCardContent')
-Card.create(content_type_id: content_type.id, content_id: text_content.id)
-
 if Rails.env.test? || Rails.env.development?
   john_doe = User.find_by(email: "customer@example.com") || FactoryGirl.create(:user, email: "customer@example.com")
   jane_doe = User.find_by(email: "adminr@example.com") || FactoryGirl.create(:user, email: "admin@example.com")
 end
+
+text_content = TextContent.create(text: 'text content woohooo!')
+content_type = ContentType.create(name: 'TextContent')
+Card.create(content_type_id: content_type.id, content_id: text_content.id, created_by: User.first.id)
+
+titled_card_content = TitledCardContent.create(title: 'super cool title', text: 'double super cool text')
+content_type = ContentType.create(name: 'TitledCardContent')
+Card.create(content_type_id: content_type.id, content_id: text_content.id, created_by: User.first.id)
