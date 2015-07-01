6e9daab6f452292121b7f83b8fec438cd949069a
Brandon Isom <brandon.g.isom@pdx.edu>
2015-07-01 08:56:56 -0700

    [#95759182] Add creation tests for text_content

--- UEBPrep/spec/models/text_content_spec.rb
+++ UEBPrep/spec/models/text_content_spec.rb
@@ -1,5 +1,18 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe TextContent, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "Create" do
+    it "Creates a corresponding Card entry" do
+      content = TextContent.new(text: "content text")
+      expect{content.save}.to change{Card.count}.by 1
+    end
+
+    it "Creates a card entry with the proper content_type_id and content_id" do
+      content = TextContent.new(text: "content text")
+      content.save
+      card = Card.find_by content_id: content.id
+      expect(card.content_id).to equal content.id
+    end
+  end
 end
