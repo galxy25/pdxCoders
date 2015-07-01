1a2fa67d953b852d0b1b4f7cf2f83a5bf4f7be17
Brandon Isom <brandon.g.isom@pdx.edu>
2015-07-01 09:05:45 -0700

    [#95759182] Added support for creation of associated card entry on save.

--- UEBPrep/app/models/titled_card_content.rb
+++ UEBPrep/app/models/titled_card_content.rb
@@ -1,5 +1,13 @@
 class TitledCardContent < ActiveRecord::Base
+  after_save :create_card
+
   def to_partial_path
     'titled_card_content'
   end
+  
+  private
+    #TODO: remove magic number somehow.
+    def create_card
+      Card.create(content_type_id: 2, content_id: self.id)
+    end
 end
