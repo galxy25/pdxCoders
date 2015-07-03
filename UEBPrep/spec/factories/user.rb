621ec3d1f2d553854d831f301d35f287cc0fc689
Brandon Isom <brandon.g.isom@pdx.edu>
2015-07-03 16:26:13 -0700

    [#94749182] Fixed up factories

--- UEBPrep/spec/factories/user.rb
+++ UEBPrep/spec/factories/user.rb
@@ -3,4 +3,9 @@ FactoryGirl.define do
     email 'example@example.com'
     password 'password'
   end
+
+  factory :user2, class: User do
+    email 'example2@example.com'
+    password 'password'
+  end
 end
