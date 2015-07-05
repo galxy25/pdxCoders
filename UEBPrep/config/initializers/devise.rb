955e863ca436d6c6d5fd5ac2857799bd0e5fdb6d
Levi Schoen <levi@scratch-it.com>
2015-07-04 21:34:35 -0700

    [#98072012] Add env variables for fb id and app secret.

--- UEBPrep/config/initializers/devise.rb
+++ UEBPrep/config/initializers/devise.rb
@@ -263,5 +263,5 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 
-  config.omniauth :facebook, "APP_ID", "APP_SECRET"
+  config.omniauth :facebook, ENV['FB_APP_ID'], ENV['FB_APP_SECRET']
 end
