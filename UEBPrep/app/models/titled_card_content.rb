c5c2021bb003d1dc0fa54800357f334cca2dfdc1
Brandon Isom <brandon.g.isom@pdx.edu>
2015-07-08 08:45:37 -0700

    [#98518400] Switched to using new so they'll actually save the card reference.

--- UEBPrep/app/models/titled_card_content.rb
+++ UEBPrep/app/models/titled_card_content.rb
@@ -12,6 +12,7 @@ class TitledCardContent < ActiveRecord::Base
   private
     #TODO: remove magic number somehow.
     def create_card
-      @card = Card.create(content_type_id: 2, content_id: self.id, created_by: self.created_by)
+      @card = Card.new(content_type_id: 2, content_id: self.id, created_by: self.created_by)
+      @card.save
     end
 end
