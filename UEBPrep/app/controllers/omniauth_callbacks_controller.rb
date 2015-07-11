32d6ab366b2478d8a35c563d03ea04bf6af2db54
Levi Schoen <levi@scratch-it.com>
2015-07-11 09:22:40 -0700

    [#98072020] Fix endless login loop -Add login with google links to signup page.

--- UEBPrep/app/controllers/omniauth_callbacks_controller.rb
+++ UEBPrep/app/controllers/omniauth_callbacks_controller.rb
@@ -6,7 +6,9 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     if @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
-      sign_in_and_redirect @user, :event => :authentication
+      # sign_in_and_redirect @user, :event => :authentication
+      sign_in(@user)
+      redirect_to profile_path
     else
       session["devise.google_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
