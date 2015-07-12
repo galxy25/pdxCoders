5a1844af3a714a21700e1a3f0e6f7022072d0d33
Levi Schoen <levi@scratch-it.com>
2015-07-11 17:14:09 -0700

    [Finishes #9807212] Add Facebook signin/account creation -TODO: Deploy UEB app live.

--- UEBPrep/config/initializers/devise.rb
+++ UEBPrep/config/initializers/devise.rb
@@ -243,7 +243,8 @@ Devise.setup do |config|
   config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
       {access_type: "offline", approval_prompt: ""}
 
-
+  config.omniauth :facebook, ENV['FB_APP_ID'], ENV['FB_APP_SECRET'] ,
+                  :scope => 'email'
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
