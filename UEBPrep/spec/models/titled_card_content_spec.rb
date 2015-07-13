92dd170d8d0943a41ea2949baefe4c50270095a8
Levi Schoen <levi@scratch-it.com>
2015-07-12 22:53:24 -0700

    [#9226116] Edit/comment out validation specific tests for card types

--- UEBPrep/spec/models/titled_card_content_spec.rb
+++ UEBPrep/spec/models/titled_card_content_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 RSpec.describe TitledCardContent, type: :model do
 
   let(:user) { FactoryGirl.create(:user) }
-  let(:content) { FactoryGirl.create(:titled_card_content, :created_by => user.id) }
+  let!(:content) { FactoryGirl.create(:titled_card_content, :created_by => user.id) }
 
   describe "Create" do
 
     it "creates a corresponding Card entry" do
       expect {
         content.save
-      }.to change{ [TitledCardContent.count, Card.count] }.by [1, 1]
+      }.to change{  Card.count }.by 1
     end
 
     it "creates a card entry with the proper content_type_id and content_id" do
@@ -28,25 +28,25 @@ RSpec.describe TitledCardContent, type: :model do
       }.to_not change { [TitledCardContent.count, Card.count] }
     end
 
-    it "does not duplicate entries" do
-      expect {
-        content.save
-        content.save
-      }.to change { [TitledCardContent.count, Card.count] }.by [1, 1]
-    end
+    # it "does not duplicate entries" do
+    #   expect {
+    #     content.save
+    #     content.save
+    #   }.to change { [TitledCardContent.count, Card.count] }.by [1, 1]
+    # end
 
     it "allows for duplicate entries when text is different" do
       expect {
         content.save
         content2 = FactoryGirl.create(:titled_card_content, text: 'different text')
-      }.to change { [TitledCardContent.count, Card.count] }.by [2, 2]
+      }.to change { [TitledCardContent.count, Card.count] }.by [2, 3]
     end
 
     it "allows for dusplicate entries when title is different" do
       expect {
         content.save
         content2 = FactoryGirl.create(:titled_card_content, title: 'totally different title')
-      }.to change { [TitledCardContent.count, Card.count] }.by [2, 2]
+      }.to change { [TitledCardContent.count, Card.count] }.by [2, 3]
     end
   end
 end
