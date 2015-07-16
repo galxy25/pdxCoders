e44551ff6f4cedcbe5f35512c75c3485d0fe3f75
Elle <ellecdwt@gmail.com>
2015-07-15 20:34:09 -0700

    Added required method for paperclip in model and path for factory girl

--- UEBPrep/app/models/image_card_content.rb
+++ UEBPrep/app/models/image_card_content.rb
@@ -1,8 +1,8 @@
 class ImageCardContent < ActiveRecord::Base
   has_attached_file :image, styles: {small: "64x64", med: "100x100", large: "200x200"}
+  validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
   after_save :create_card
   validates :text, presence: true
-  validates_uniqueness_of :scope => :text
 
   attr_accessor :created_by
   attr_reader :card
