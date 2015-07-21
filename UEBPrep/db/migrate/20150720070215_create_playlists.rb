dbf3d32b8b5f4a3f896357249622b87f624db116
Brady Sullivan <brady@bsull.com>
2015-07-21 11:05:11 -0700

    Add join tables to migration.

--- UEBPrep/db/migrate/20150720070215_create_playlists.rb
+++ UEBPrep/db/migrate/20150720070215_create_playlists.rb
@@ -4,5 +4,16 @@ class CreatePlaylists < ActiveRecord::Migration
         t.string :name
 
     end
+
+    create_table :cards_playlist do |t|
+        t.int :playlist_id  # Foreign key to playlists
+        t.int :card_id      # Foreign key to cards
+        t.int :order        # Int to determine ordering of cards
+    end
+
+    create_table :playlists_user do |t|
+        t.int :playlist_id  # Fkey to playlists
+        t.int :user_id      # Fkey to users
+    end
   end
 end
