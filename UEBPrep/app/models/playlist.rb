cce74399e09f268008ee5f36ff196135019ba310
Brady Sullivan <brady@bsull.com>
2015-07-21 11:43:55 -0700

    Update migration to actually work.

--- UEBPrep/app/models/playlist.rb
+++ UEBPrep/app/models/playlist.rb
@@ -2,4 +2,5 @@ class Playlist < ActiveRecord::Base
     validates :name, presence: true
 
     belongs_to :user
+    has_many :cards, through: :cards_playlists
 end
--- UEBPrep/db/migrate/20150720070215_create_playlists.rb
+++ UEBPrep/db/migrate/20150720070215_create_playlists.rb
@@ -1,19 +1,24 @@
 class CreatePlaylists < ActiveRecord::Migration
   def change
+
+    ActiveSupport.on_load :active_record do
+        Foreigner.load
+    end
+
     create_table :playlists do |t|
         t.string :name
 
     end
 
-    create_table :cards_playlist do |t|
-        t.int :playlist_id  # Foreign key to playlists
-        t.int :card_id      # Foreign key to cards
-        t.int :order        # Int to determine ordering of cards
+    create_table :cards_playlists do |t|
+        t.integer :order  # Int to determine ordering of cards
+        t.integer :card_id
+        t.integer :playlist_id
     end
 
-    create_table :playlists_user do |t|
-        t.int :playlist_id  # Fkey to playlists
-        t.int :user_id      # Fkey to users
-    end
+    add_foreign_key :cards_playlists, :cards
+    add_foreign_key :cards_playlists, :playlists
+
+    create_join_table :playlists, :users
   end
 end
