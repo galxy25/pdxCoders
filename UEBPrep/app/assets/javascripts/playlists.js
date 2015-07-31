17e50f7938fee64d1fc9fab148b694a77b936aa1
Elle <ellecdwt@gmail.com>
2015-07-30 19:29:35 -0700

    jquery ui for dialog
    the dialog now pops up. now need to have form render in it for playlist
    edit.

--- UEBPrep/app/assets/javascripts/playlists.js
+++ UEBPrep/app/assets/javascripts/playlists.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
-    $("#user_playlists").on("click", ".user_playlist", function(event){
-        alert('clicked');
+    $("#user_playlists li").on("click", function(){
+        $('#edit_playlist_dialog').dialog({
+            autoOpen: false,
+            draggable: true,
+            modal: true,
+            resizable: false,
+            title: "Edit Playlist: " + $(this).text(),
+            width: 750,
+            buttons: {
+                Edit: function() {alert("submit edit")},
+                Cancel: function() {$('#edit_playlist_dialog').dialog("close"); $('#edit_playlist_dialog').empty();}
+            }
+        }).dialog("open");
     });
 })
 
