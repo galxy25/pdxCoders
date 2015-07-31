9e9791edebeef3f28bd4688e5fb219865ee2daac
Elle <ellecdwt@gmail.com>
2015-07-30 20:12:12 -0700

    sortable
    added a sortable to the dialog. If we can populate this with the
    selected playlist's cards, the user can just drag them around in the
    order they want them

--- UEBPrep/app/assets/javascripts/playlists.js
+++ UEBPrep/app/assets/javascripts/playlists.js
@@ -8,10 +8,15 @@ $(document).ready(function(){
             title: "Edit Playlist: " + $(this).text(),
             width: 750,
             buttons: {
-                Edit: function() {alert("submit edit")},
+                Save: function() {alert("submit edit")},
                 Cancel: function() {$('#edit_playlist_dialog').dialog("close"); $('#edit_playlist_dialog').empty();}
             }
-        }).dialog("open");
+        }).dialog("open").css('display', 'block');
+
+        $('#sortable').sortable({
+            axis: "y",
+            cursor: "move"
+        });
     });
 })
 
