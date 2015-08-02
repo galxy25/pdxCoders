c353721818c216b8d2b87635b8fcd10b0418443a
Elle <ellecdwt@gmail.com>
2015-08-02 13:38:54 -0700

    Added playlist title to the edit view
    The playlist title is now part of the edit view. This needs to be sent
    in as well as the cards and their order

--- UEBPrep/app/assets/javascripts/playlists.js
+++ UEBPrep/app/assets/javascripts/playlists.js
@@ -8,8 +8,9 @@ $(document).ready(function(){
             title: "Edit Playlist: " + $(this).text(),
             width: 750,
             buttons: {
-                Save: function() {alert("submit edit"); var idsInOrder = $("#sortable2").sortable("toArray"); console.log(idsInOrder);},
-                Cancel: function() {$('#edit_playlist_dialog').dialog("close"); $('#edit_playlist_dialog').empty();}
+                Save: function() {alert("submit edit"); var idsInOrder = $("#sortable2").sortable("toArray"); console.log(idsInOrder);
+                    $('#edit_playlist_dialog').dialog("close");},
+                Cancel: function() {$('#edit_playlist_dialog').dialog("close");}
             }
         }).dialog("open").css('display', 'block');
 
@@ -17,6 +18,11 @@ $(document).ready(function(){
             axis: "y",
             cursor: "move"
         });
+
+        var title = $(this).text();
+        var splits = title.split('(');
+
+        $('#playlist_title').val(splits[0].trim());
     });
 })
 
