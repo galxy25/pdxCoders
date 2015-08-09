bc440ced378f2bcf0ae780a86c0de262ade27c45
caesarfish <schottec@gmail.com>
2015-08-09 07:46:43 -0700

    breaks user sign-up

--- UEBPrep/config/initializers/devise.rb
+++ UEBPrep/config/initializers/devise.rb
@@ -52,7 +52,7 @@ Devise.setup do |config|
   # modifying a user and when used to authenticate or find a user. Default is :email.
   # config.strip_whitespace_keys = [:email]
   config.strip_whitespace_keys = [:username]
-  
+
 
   # Tell if authentication through request.params is enabled. True by default.
   # It can be set to an array that will enable params authentication only for the
