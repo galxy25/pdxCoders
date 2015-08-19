ab8c19f437b813cabd9bca4ac3865fb390ed98a3
caesarfish <schottec@gmail.com>
2015-08-19 08:30:38 -0700

    generifies facebook login

--- UEBPrep/app/controllers/omniauth_callbacks_controller.rb
+++ UEBPrep/app/controllers/omniauth_callbacks_controller.rb
@@ -1,14 +1,15 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
     auth = request.env["omniauth.auth"]
-    @user = User.from_omniauth(auth)
     _provider = auth.provider.split("_")[0].capitalize
+
+    @user = User.from_omniauth(auth)
     if @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => _provider
       sign_in(@user)
       redirect_to profile_path
     else
-      if auth.provder.equal?("google")
+      if _provider.equal?("Google")
         session["devise.google_data"] = request.env["omniauth.auth"]
       end
       redirect_to signup_path
@@ -16,13 +17,18 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def facebook
+    auth = request.env["omniauth.auth"]
+    _provider = auth.provider.split("_")[0].capitalize
+
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => _provider
       sign_in(@user)
       redirect_to profile_path
     else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      if _provider.equal?("Facebook")
+        session["devise.facebook_data"] = request.env["omniauth.auth"]
+      end
       redirect_to signup_path
     end
   end
