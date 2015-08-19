6fecf44bc076a4f3545667c2589b0095445755dc
caesarfish <schottec@gmail.com>
2015-08-19 08:43:46 -0700

    refactors all social logins to single callback method

--- UEBPrep/app/controllers/omniauth_callbacks_controller.rb
+++ UEBPrep/app/controllers/omniauth_callbacks_controller.rb
@@ -1,5 +1,6 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def google_oauth2
+
+  def provider_login
     auth = request.env["omniauth.auth"]
     _provider = auth.provider.split("_")[0].capitalize
 
@@ -11,40 +12,24 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     else
       if _provider.equal?("Google")
         session["devise.google_data"] = request.env["omniauth.auth"]
-      end
-      redirect_to signup_path
-    end
-  end
-
-  def facebook
-    auth = request.env["omniauth.auth"]
-    _provider = auth.provider.split("_")[0].capitalize
-
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-    if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => _provider
-      sign_in(@user)
-      redirect_to profile_path
-    else
-      if _provider.equal?("Facebook")
+      elsif _provider.equal?("Facebook")
         session["devise.facebook_data"] = request.env["omniauth.auth"]
-      end
-      redirect_to signup_path
-    end
-  end
-
-  def twitter
-    auth = request.env["omniauth.auth"]
-    @user = User.from_omniauth(auth)
-    if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => auth.provider.capitalize
-      sign_in(@user)
-      redirect_to profile_path
-    else
-      if auth.provider.equal?("twitter")
+      elsif _provider.equal?("Twitter")
         session["devise.twitter_uid"] = request.env["omniauth.auth"]
       end
       redirect_to signup_path
     end
+  end
+
+  def google_oauth2
+    provider_login
+  end
+
+  def facebook
+    provider_login
+  end
+
+  def twitter
+    provider_login
   end
 end
